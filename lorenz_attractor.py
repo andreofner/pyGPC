898124dfff4cee2c69a9e7084c381e5cbc477931
Andre Ofner <andreofner@users.noreply.github.com>
2022-02-19 11:48:33 +0100

    Create lorenz_attractor.py

---
+++ lorenz_attractor.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lorenz(x, y, z, s=10, r=28, b=2.667):
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
+    return x_dot, y_dot, z_dot
+
+
+def generate_data(num_steps = 100, dt = 0.01, plot=False, initial=(0., 1., 1.05)):
+    # Need one more for the initial values
+    xs = np.empty(num_steps + 1)
+    ys = np.empty(num_steps + 1)
+    zs = np.empty(num_steps + 1)
+
+    # Set initial values
+    xs[0], ys[0], zs[0] = initial
+
+    # Step through "time", calculating the partial derivatives at the current point
+    # and using them to estimate the next point
+    for i in range(num_steps):
+        x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i])
+        xs[i + 1] = xs[i] + (x_dot * dt)
+        ys[i + 1] = ys[i] + (y_dot * dt)
+        zs[i + 1] = zs[i] + (z_dot * dt)
+
+    # Plot
+    if plot:
+        ax = plt.figure().add_subplot(projection='3d')
+
+        ax.plot(xs, ys, zs, lw=0.5)
+        ax.set_xlabel("X Axis")
+        ax.set_ylabel("Y Axis")
+        ax.set_zlabel("Z Axis")
+        ax.set_title("Lorenz Attractor")
+
+        plt.show()
+
+    return xs, ys, zs
+
+
